c534774e4d5640e1d3ebd6b00b51eb4c4f39c1cc
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2023-04-14 21:32:11 +0200

    added all files again

---
+++ register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Create your account</title>
+</head>
+<body>
+    <div class="promptsSignIn">
+        <form action="" method="post">
+            <h1>Create your account</h1>
+            <hr>
+            <div class="form__field">
+					<label for="Username">Username</label><br>
+					<input type="text" id="username" name="username">
+			</div>
+            <div class="form__field">
+					<label for="Email">Email</label><br>
+					<input type="text" id="email" name="email">
+			</div>
+
+			<div class="form__field">
+					<label for="Password">Password</label><br>
+					<input type="password" id="password" name="password">
+			</div>
+            <br>
+            <div>
+                <button type="submit">Create my account</button>
+            </div>
+            
+        </form>
+    </div>
+   
+</body>
+</html>
