e7d24e479567259e1a1b9fcf04b0ec0902b5f62e
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2023-04-17 17:30:36 +0200

    feat: style.css updated

--- register.php
+++ register.php
@@ -7,7 +7,6 @@
             'cost' => 14,
         ];
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT, $options);
-        
         //echo $password;
 
         $conn = new PDO('mysql:host=127.0.0.1;dbname=phpendproject',"root", "root");
