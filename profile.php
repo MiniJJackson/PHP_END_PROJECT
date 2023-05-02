2cca85ee7d7bb26a86cad3923055a3c2b826d37b
Sidney <quinten.geeraerts@student.ucll.be>
2023-05-02 11:52:39 +0200

    feat: added edit profile, profile and styles

---
+++ profile.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
+    <title>My profile</title>
+</head>
+<body>
+    <h1 class="heading1">My profile</h1>
+
+    <div>
+        <img src="https://static.vecteezy.com/system/resources/previews/001/851/269/original/luxury-business-banner-background-free-vector.jpg" alt="Banner" class="banner">
+    </div>
+
+    <div>
+      <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="profile-picture">
+        <div class="info">
+        <h2 class="full-name">Full name</h1>
+        <h3 class="username">Username</h2>
+        <p class="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nulla magna, tempus ac varius non, ultrices in tellus. Suspendisse nec metus magna. Quisque interdum turpis turpis, dapibus egestas ante pharetra ut.</p>
+        </div>
+    </div>
+
+    <div>
+        <a class="small-btn" href="edit-profile.php">Edit profile</a>
+    </div>
+
+        <div class="heading2">
+        <h4 class="heading2-text">My prompts</h4>
+        </div>
+
+        <div class="prompts">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image1">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image2">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image3">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image4">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image5">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image6">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image7">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image8">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image9">
+        <img src="https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg" alt="cat" class="prompts-image10">
+        </div>
+
+        <div class="heading3">
+        <h4 class="heading2-text">My favorite prompts</h4>
+        </div>
+
+        <div class="favprompts">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image1">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image2">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image3">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image4">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image5">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image6">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image7">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image8">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image9">
+        <img src="https://www.rd.com/wp-content/uploads/2021/06/GettyImages-1205998407-e1624731290663.jpg?fit=700,1024" alt="dog" class="favprompts-image10">
+        </div>
+
+
+</body>
+</html>
